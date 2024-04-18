6115f9e2cb5ed64de6ef928ca115a3070d5420c5
utkarsh4320 <161574007+utkarsh4320@users.noreply.github.com>
2024-04-18 09:57:38 +0000

    any message

--- capstone_streamlit.py
+++ capstone_streamlit.py
@@ -17,21 +17,15 @@ st.set_page_config(page_title="UTKARSH ANAND - Customer Segmentation Analysis", 
 st.header("Customer Segmentation Analysis for Retail")
 
 # Load data
-df = pd.DataFrame()
-# Load data
 st.title("CSV File Uploader")
 # File uploader widget
-uploaded_file = st.file_uploader("Choose a CSV file", type=["csv"])
-if uploaded_file is not None:
-    # Read the uploaded CSV file
-    df = pd.read_csv(uploaded_file, encoding='latin-1')
-    # Display the DataFrame
-    st.write("Uploaded DataFrame:", df)
+
 
 # Continue with the rest of your script...
 
 
-# df = pd.read_csv('https://github.com/utkarsh4320/Capstone_Dataset/blob/main/Capstone_Final_data%20(1).csv',encoding='latin-1')  
+df = pd.read_csv('https://github.com/utkarsh4320/Capstone_Dataset/blob/main/Capstone_Final_data%20(1).csv',encoding='latin-1')  
+
 
 
 df['dim_Order_Date'] = pd.to_datetime(df['dim_Order_Date'],format="%d-%m-%Y")
@@ -45,6 +39,7 @@ visits_per_month = df.groupby('month').size().reset_index(name='visits_per_month
 print(visits_per_month)
 
 df=pd.merge(df,visits_per_month,how="left",on="month")
+
 # Label encode categorical columns
 le = LabelEncoder()
 cat_cols=['dim_Ship_Mode','dim_Customer_Name','dim_Region','dim_Product_Category','dim_Product_Sub_Category','dim_Product_Name','dim_Gender','dim_Store_type','dim_Payment_Mode',
